ebae1ee30123194692088e91a9c78fdbe42fb206
HxLu03 <leo0306lu@gmail.com>
2025-05-04 17:36:02 +1000

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  // github pages deployment
-  base: "/",
+  base: "/EPortfolio/",
   plugins: [react(), tailwindcss()],
 });
