fccb0f367fcdea9ee930498e3048f042c4283d9b
HxLu03 <leo0306lu@gmail.com>
2025-05-04 17:31:04 +1000

    Revert to 8411244fa55227b9cccf8208c613af5af4a060f4

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   // github pages deployment
-  base: "/EPortfolio/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
