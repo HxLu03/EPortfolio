54357d51bfb49678ed5fbbfdaa751aa1154684d1
HxLu03 <leo0306lu@gmail.com>
2025-05-04 19:09:09 +1000

    refactor footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,4 +1,37 @@
-import { mySocials } from "../constants";
+const mySocials = [
+  {
+    name: "GitHub",
+    href: "https://github.com/HxLu03",
+    icon: "/assets/socials/github.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/haoxuanlu/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/leo0306_/",
+    icon: "/assets/socials/instagram.svg",
+  },
+  {
+    name: "WeChat",
+    // pops up a QR code for WeChat
+    href: "https://www.wechat.com/en/",
+    icon: "/assets/socials/WeChat.svg",
+  },
+  {
+    name: "Tiktok CN",
+    href: "https://v.douyin.com/0QFWZqaLEuw/",
+    icon: "/assets/socials/douyin.svg",
+  },
+  {
+    name: "xiaohongshu",
+    href: "https://www.xiaohongshu.com/user/profile/5d470775000000001603562b?xsec_token=YBydf5CQz9iNGHbQMMYT8WATbxR-JKRPUDEqglfs4WzDo%3D&xsec_source=app_share&xhsshare=CopyLink&appuid=5d470775000000001603562b&apptime=1746332167&share_id=9df882e412dd4b31a05cd2eecb4ea54a&share_channel=copy_link/",
+    icon: "https://upload.wikimedia.org/wikipedia/commons/c/c1/XiaohongshuLOGO.svg",
+  }
+];
+
 const Footer = () => {
   return (
     <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
