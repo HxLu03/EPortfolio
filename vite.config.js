bc290ee06ed44c63f42a917cff2e412a9c73aae7
HxLu03 <leo0306lu@gmail.com>
2025-05-04 17:16:35 +1000

    Prepare site for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   // github pages deployment
-  base: "/",
+  base: "/EPortfolio/",
   plugins: [react(), tailwindcss()],
 });
